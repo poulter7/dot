c156be63105c61236f558ebbea07a4b21883076e
Jonathan Poulter <jonpoult@amazon.com>
2022-11-18 15:58:51 -0500

    Update cd and add neoclip

--- lvim/config.lua
+++ lvim/config.lua
@@ -18,14 +18,8 @@ lvim.colorscheme = "lunar"
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-local nvim_tmux_nav = require('nvim-tmux-navigation')
-
-lvim.keys.normal_mode["<C-h>"] = nvim_tmux_nav.NvimTmuxNavigateLeft
-lvim.keys.normal_mode["<C-j>"] = nvim_tmux_nav.NvimTmuxNavigateDown
-lvim.keys.normal_mode["<C-k>"] = nvim_tmux_nav.NvimTmuxNavigateUp
-lvim.keys.normal_mode["<C-l>"] = nvim_tmux_nav.NvimTmuxNavigateRight
-lvim.keys.normal_mode["<C-\\>"] = nvim_tmux_nav.NvimTmuxNavigateLastActive
-lvim.keys.normal_mode["<C-Space>"] = nvim_tmux_nav.NvimTmuxNavigateNext
+vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
@@ -70,7 +64,7 @@ lvim.keys.normal_mode["<C-Space>"] = nvim_tmux_nav.NvimTmuxNavigateNext
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.mode = "startify"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
@@ -177,6 +171,60 @@ lvim.plugins = {
   {"nvim-lua/plenary.nvim"},
   {"tpope/vim-obsession"},
   {"jvgrootveld/telescope-zoxide"},
+  {'kevinhwang91/nvim-bqf', ft = 'qf'},
+  {
+      "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+    position = "bottom", -- position of the list can be: bottom, top, left, right
+    height = 10, -- height of the trouble list when position is top or bottom
+    width = 50, -- width of the list when position is left or right
+    icons = true, -- use devicons for filenames
+    mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+    fold_open = "", -- icon used for open folds
+    fold_closed = "", -- icon used for closed folds
+    group = true, -- group results by file
+    padding = true, -- add an extra new line on top of the list
+    action_keys = { -- key mappings for actions in the trouble list
+        -- map to {} to remove a mapping, for example:
+        -- close = {},
+        close = "q", -- close the list
+        cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r", -- manually refresh
+        jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" }, -- open buffer in new split
+        open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+        open_tab = { "<c-t>" }, -- open buffer in new tab
+        jump_close = {"o"}, -- jump to the diagnostic and close the list
+        toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        toggle_preview = "P", -- toggle auto_preview
+        hover = "K", -- opens a small popup with the full multiline message
+        preview = "p", -- preview the diagnostic location
+        close_folds = {"zM", "zm"}, -- close all folds
+        open_folds = {"zR", "zr"}, -- open all folds
+        toggle_fold = {"zA", "za"}, -- toggle fold of current file
+        previous = "k", -- previous item
+        next = "j" -- next item
+    },
+    indent_lines = true, -- add an indent guide below the fold icons
+    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    auto_fold = false, -- automatically fold a file trouble list at creation
+    auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+    signs = {
+        -- icons / text used for a diagnostic
+        error = "",
+        warning = "",
+        hint = "",
+        information = "",
+        other = "﫠"
+    },
+    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+}
+    end
+  },
   {"mrjones2014/dash.nvim", run="make install" },
   {
     'phaazon/hop.nvim',
@@ -187,6 +235,19 @@ lvim.plugins = {
       vim.api.nvim_set_keymap("n", "f", ":HopChar2<cr>", { silent = true })
       vim.api.nvim_set_keymap("n", "F", ":HopWord<cr>", { silent = true })
     end
+  },
+  {
+    "AckslD/nvim-neoclip.lua",
+    requires = {
+      {'kkharji/sqlite.lua', module = 'sqlite'},
+      -- you'll need at least one of these
+      -- {'nvim-telescope/telescope.nvim'},
+      -- {'ibhagwan/fzf-lua'},
+    },
+    config = function()
+      require('neoclip').setup()
+    end
+
   }
 }
 lvim.keys.normal_mode['<leader><leader>f']=":Telescope find_files<CR>"
@@ -198,8 +259,18 @@ lvim.builtin.telescope.on_config_done = function(telescope)
   pcall(telescope.load_extension, "frecency")
   pcall(telescope.load_extension, "neoclip")
   pcall(telescope.load_extension, "zoxide")
+  pcall(telescope.load_extension, "trouble")
   -- any other extensions loading
 end
+
+local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+lvim.keys.normal_mode["<C-h>"] = nvim_tmux_nav.NvimTmuxNavigateLeft
+lvim.keys.normal_mode["<C-j>"] = nvim_tmux_nav.NvimTmuxNavigateDown
+lvim.keys.normal_mode["<C-k>"] = nvim_tmux_nav.NvimTmuxNavigateUp
+lvim.keys.normal_mode["<C-l>"] = nvim_tmux_nav.NvimTmuxNavigateRight
+lvim.keys.normal_mode["<C-\\>"] = nvim_tmux_nav.NvimTmuxNavigateLastActive
+lvim.keys.normal_mode["<C-Space>"] = nvim_tmux_nav.NvimTmuxNavigateNext
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
