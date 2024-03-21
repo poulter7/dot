e1383a56d6edc049c5a43b410d8f20fda0dcfbeb
Jonathan Poulter <poulter7@gmail.com>
2024-03-20 23:13:00 -0500

    add vim distributions

--- install/mac/nvim.sh
+++ install/mac/nvim.sh
@@ -1,6 +1,8 @@
 rm -rf ~/.local/share/nvim-**
 rm -rf ~/.local/state/nvim-**
+rm -rf ~/.config/nvim-**
 brew install nvim
-git clone https://github.com/NvChad/starter ~/.config/nvim-nvchad
-git clone https://github.com/LazyVim/starter ~/.config/nvim-lazyvim
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim-astronvim
+ln -s ~/dot/apps/nvim-astronvim ~/.config/nvim-astronvim
+ln -s ~/dot/apps/nvim-astronvim-user ~/.config/nvim-astronvim/lua/user
+ln -s ~/dot/apps/nvim-lazyvim ~/.config/nvim-lazyvim
+ln -s ~/dot/apps/nvim-nvchad ~/.config/nvim-nvchad
