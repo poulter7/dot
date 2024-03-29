2ae7d4176177bae8d9fb8495aa3cac3af5fe6e2b
Jonathan Poulter <poulter7@gmail.com>
2024-03-29 13:19:04 -0500

    Update saml2aws-linux.sh

--- install/saml2aws-linux.sh
+++ install/saml2aws-linux.sh
@@ -1,1 +1,6 @@
-
+CURRENT_VERSION=$(curl -Ls https://api.github.com/repos/Versent/saml2aws/releases/latest | grep 'tag_name' | cut -d'v' -f2 | cut -d'"' -f1)
+wget https://github.com/Versent/saml2aws/releases/download/v${CURRENT_VERSION}/saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz
+tar -xzvf saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz
+mv saml2aws /usr/local/bin/
+chmod u+x /usr/local/bin/saml2aws
+saml2aws --version
