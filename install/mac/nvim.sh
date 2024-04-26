11a0f81acd10e9f3b6c4da68207534ad0479b897
Jonathan Poulter <poulter7@gmail.com>
2024-04-26 08:57:53 -0500

    Update nvim.sh

--- install/mac/nvim.sh
+++ install/mac/nvim.sh
@@ -1,10 +1,10 @@
 rm -rf ~/.local/share/nvim-**
 rm -rf ~/.local/state/nvim-**
 rm -rf ~/.config/nvim-**
-brew install gnu-sed # needed for Spectre
-brew install nvim
 ln -s ~/dot/apps/nvim-astronvim ~/.config/nvim-astronvim
 ln -s ~/dot/apps/nvim-astronvim-user ~/.config/nvim-astronvim/lua/user
 ln -s ~/dot/apps/nvim-astronvim-v4 ~/.config/nvim-astronvim-v4
 ln -s ~/dot/apps/nvim-lazyvim ~/.config/nvim-lazyvim
 ln -s ~/dot/apps/nvim-nvchad ~/.config/nvim-nvchad
+brew install gnu-sed # needed for Spectre
+brew install nvim
